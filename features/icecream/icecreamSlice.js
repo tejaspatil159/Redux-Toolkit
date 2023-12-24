e150c1d6c9f502f5cfb90333114c310359714b0a
tejaspatil159 <117001605+tejaspatil159@users.noreply.github.com>
2023-12-24 23:35:36 +0530

    adding icecreamSlice

---
+++ features/icecream/icecreamSlice.js
@@ -0,0 +1,21 @@
+const createSlice = require("@reduxjs/toolkit").createSlice;
+
+const initialState = {
+  noOfIcecreams: 20,
+};
+
+const icecreamSlice = createSlice({
+  name: "icecream",
+  initialState,
+  reducers: {
+    ordered: (state) => {
+      state.noOfIcecreams--;
+    },
+    restocked: (state, action) => {
+      state.noOfIcecreams += action.payload;
+    },
+  },
+});
+
+module.exports = icecreamSlice.reducer;
+module.exports.icecreamActions = icecreamSlice.actions;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
+const icecreamActions =
+  require("./features/icecream/icecreamSlice").icecreamActions;
 
 console.log(`Initial state`, store.getState());
 const unSubscribed = store.subscribe(() => {
@@ -8,5 +10,7 @@ const unSubscribed = store.subscribe(() => {
 
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.restocked(2));
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.restocked(2));
 
 unSubscribed();
