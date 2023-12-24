5e0d7874a61d839b4dd8728d05d985db05424990
tejaspatil159 <117001605+tejaspatil159@users.noreply.github.com>
2023-12-25 01:00:46 +0530

    async Action using createAsyncThunk to fetch users data frpm jsonplaceholder

--- app/store.js
+++ app/store.js
@@ -2,11 +2,13 @@ const configureStore = require("@reduxjs/toolkit").configureStore;
 const reduxLogger = require("redux-logger").createLogger();
 const cakeReducer = require("../features/cake/cakeSlice");
 const icecreamReducer = require("../features/icecream/icecreamSlice");
+const userReducer = require("../features/user/userSlice");
 
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     icecream: icecreamReducer,
+    user: userReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(reduxLogger),
--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
 const icecreamActions =
   require("./features/icecream/icecreamSlice").icecreamActions;
+const fetchUsers = require("./features/user/userSlice").fetchUsers;
 
 console.log(`Initial state`, store.getState());
 const unSubscribed = store.subscribe(() => {});
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(2));
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.restocked(2));
-
-unSubscribed();
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(2));
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.restocked(2));
+store.dispatch(fetchUsers());
